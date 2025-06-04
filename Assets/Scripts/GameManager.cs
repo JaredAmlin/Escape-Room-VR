5125b2762597aaffe919bd1637da3908663f0ee4
JaredAmlin <jaredamlin@gmail.com>
2025-06-04 23:19:27 +0100

    Finished Project
    UI, Bug Fixes, Game Loop

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -9,12 +10,18 @@ public class GameManager : MonoBehaviour
 
     [SerializeField] private int _locksUnlocked;
 
+    [SerializeField] private Canvas _screenSpaceCanvas;
+
     [SerializeField] UnityEvent _onUnlock;
+
+    [SerializeField] UnityEvent _onGameComplete;
 
     // Start is called before the first frame update
     void Start()
     {
         LightBlade.onRoomComplete += LightBlade_onRoomComplete;
+
+        StartCoroutine(DisableCanvasRoutine());
     }
 
     private void OnDisable()
@@ -24,13 +31,7 @@ public class GameManager : MonoBehaviour
 
     private void LightBlade_onRoomComplete()
     {
-        OnGameComplete();
-    }
-
-    private void OnGameComplete()
-    {
-        if(_exitDoorAnimatior != null)
-            _exitDoorAnimatior.SetTrigger("OpenDoor");
+        _onGameComplete.Invoke();
     }
 
     public void Unlock()
@@ -42,5 +43,23 @@ public class GameManager : MonoBehaviour
             //activate secret box
             _onUnlock.Invoke();
         }
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void ReloadLevel()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    private IEnumerator DisableCanvasRoutine()
+    {
+        yield return new WaitForSeconds(8f);
+
+        if (_screenSpaceCanvas != null)
+            _screenSpaceCanvas.enabled = false;
     }
 }
--- Assets/Scripts/LightBlade.cs
+++ Assets/Scripts/LightBlade.cs
@@ -6,6 +6,8 @@ using System;
 public class LightBlade : MonoBehaviour
 {
     [SerializeField] private ParticleSystem _lightParticles;
+    [SerializeField] private ParticleSystem _redBurstBarticles;
+    [SerializeField] private AudioSource _audioSource;
 
     public static event Action onRoomComplete;
 
@@ -16,7 +18,6 @@ public class LightBlade : MonoBehaviour
             if (!_lightParticles.isPlaying)
             {
                 _lightParticles.Play();
-                //_lightParticles.transform.position = other.ClosestPoint(_lightParticles.transform.position);
             }
         }
     }
@@ -34,6 +35,16 @@ public class LightBlade : MonoBehaviour
         {
             if(other.gameObject != null)
             {
+                if (_redBurstBarticles != null)
+                {
+                    _redBurstBarticles.Play();
+                }
+
+                if (_audioSource != null)
+                {
+                    _audioSource.Play();
+                }
+
                 other.gameObject.SetActive(false);
                 //trigger door opening and end game scenario
                 onRoomComplete?.Invoke();
